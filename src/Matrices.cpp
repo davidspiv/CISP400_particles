705f6df126d57ba267eeaaea2e8bfbd55bdd65f2
davidspiv <daspivack@gmail.com>
2025-05-03 17:18:41 -0700

    completed matrix operations

--- src/Matrices.cpp
+++ src/Matrices.cpp
@@ -1,6 +1,7 @@
 #include "Matrices.h"
 
 #include <iomanip>
+#include <stdexcept>
 #include <vector>
 
 Matrix::Matrix(int rows, int cols)
@@ -8,27 +9,35 @@ Matrix::Matrix(int rows, int cols)
           std::vector<std::vector<double>>(rows, std::vector<double>(cols, 0))),
       m_rows(rows), m_cols(cols) {};
 
-std::ostream &operator<<(std::ostream &os, const Matrix &a) {
-  for (size_t row = 0; row < a.getRows(); row++) {
-    for (size_t col = 0; col < a.getRows(); col++) {
-      os << std::setw(10) << a(row, col) << ' ';
-    }
-    os << '\n';
+
+Matrix operator+(const Matrix &a, const Matrix &b) {
+  if (a.rows() != b.rows() || a.cols() != b.cols()) {
+    throw std::domain_error("Error: mismatched dimensions");
   }
-  return os;
+
+  Matrix result(a.rows(), a.cols());
+
+  for (size_t i = 0; i < a.rows(); i++) {
+    for (size_t j = 0; j < a.cols(); j++) {
+      result(i, j) = a(i, j) + b(i, j);
+    }
+  }
+
+  return result;
 }
 
+
 Matrix operator*(const Matrix &a, const Matrix &b) {
-  if (a.getCols() != b.getRows()) {
-    throw std::runtime_error("Error: mismatched inner dimensions");
+  if (a.cols() != b.rows()) {
+    throw std::domain_error("Error: mismatched inner dimensions");
   }
 
-  Matrix result(a.getRows(), b.getCols());
+  Matrix result(a.rows(), b.cols());
 
-  for (size_t i = 0; i < a.getRows(); i++) {
-    for (size_t k = 0; k < b.getCols(); k++) {
+  for (size_t i = 0; i < a.rows(); i++) {
+    for (size_t k = 0; k < b.cols(); k++) {
       float sum = 0;
-      for (size_t j = 0; j < a.getCols(); j++) {
+      for (size_t j = 0; j < a.cols(); j++) {
         sum += a(i, j) * b(j, k);
       }
       result(i, k) = sum;
@@ -36,4 +45,37 @@ Matrix operator*(const Matrix &a, const Matrix &b) {
   }
 
   return result;
+}
+
+
+bool operator==(const Matrix &a, const Matrix &b) {
+  if (a.rows() != b.rows() || a.cols() != b.cols()) {
+    return false;
+  }
+
+  Matrix result(a.rows(), a.cols());
+
+  for (size_t i = 0; i < a.rows(); i++) {
+    for (size_t j = 0; j < a.cols(); j++) {
+      if (a(i, j) != b(i, j)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+
+bool operator!=(const Matrix &a, const Matrix &b) { return !(a == b); }
+
+
+std::ostream &operator<<(std::ostream &os, const Matrix &a) {
+  for (size_t row = 0; row < a.rows(); row++) {
+    for (size_t col = 0; col < a.rows(); col++) {
+      os << std::setw(10) << a(row, col) << ' ';
+    }
+    os << '\n';
+  }
+  return os;
 }
