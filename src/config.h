90fc162331193eba00efadb69a00751f62b1b8e6
davidspiv <daspivack@gmail.com>
2025-05-05 21:35:21 -0700

    visible shrinking

--- src/config.h
+++ src/config.h
@@ -8,6 +8,6 @@ constexpr uint8_t TARGET_FPS = 60;
 
 constexpr float G = 1000;  // Gravity
 constexpr float TTL = 5.0; // Time To Live
-constexpr float SCALE = 0.999;
+constexpr float SCALE = 0.99;
 
 const sf::Color WHITE = sf::Color(255l, 255l, 255l);
